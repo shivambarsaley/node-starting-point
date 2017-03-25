02d8d3cfacc442c00952e6cd4f272e607230a485
shivam barsaley <shivam_barsaley@mckinsey.com>
2017-03-25 19:12:03 +0530

    Updated the ReadMe Refactored to use KeyWord instead of symbols for operators

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ node-starting-point
 ===
 this is to bootstrap a simple node based application with test. 
 
+This contains a simple calcualor supporting Add Operation:
+
+It also has test cases for the same
+
+Performing a simple Add operation: `npm start ADD 3 5`
+
+Running a test case: `npm test`
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var Calculator = require('./src/calculator');
 
 (() => {
-  var operation = process.argv[3];
-  var operand1 = Number(process.argv[2]);
+  var operation = process.argv[2].toUpperCase();
+  var operand1 = Number(process.argv[3]);
   var operand2 = Number(process.argv[4]);
   // [,,operand1,operation,operand2] = process.argv
   switch(operation){
-    case '+':
+    case 'ADD':
     // console.log(Calculator.sum(+operand1, +operand2));
     console.log(Calculator.sum(operand1, operand2));
     break;
